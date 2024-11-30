d6b5d56266fa8f20ee13218e6d293565fdd606e1
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2024-11-30 22:53:06 +0000

    1.20.6 port
    initial commit

--- src/client/java/sircow/noworldbordertint/mixin/client/InGameHudMixin.java
+++ src/client/java/sircow/noworldbordertint/mixin/client/InGameHudMixin.java
@@ -1,11 +1,11 @@
 package sircow.noworldbordertint.mixin.client;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,19 +21,34 @@ public class InGameHudMixin {
 
     @Inject(method = "renderVignetteOverlay", at = @At("HEAD"), cancellable = true)
     private void sir_cow$modifyVignette(DrawContext context, Entity entity, CallbackInfo ci) {
-        int i;
-        float h = this.vignetteDarkness;
-        h = MathHelper.clamp(h, 0.0F, 1.0F);
-        i = ColorHelper.fromFloats(1.0F, h, h, h);
+        RenderSystem.disableDepthTest();
+        RenderSystem.depthMask(false);
+        RenderSystem.enableBlend();
+        RenderSystem.blendFuncSeparate(
+                GlStateManager.SrcFactor.ZERO, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO
+        );
+
+        float g = this.vignetteDarkness;
+        g = MathHelper.clamp(g, 0.0F, 1.0F);
+        context.setShaderColor(g, g, g, 1.0F);
 
         context.drawTexture(
-                RenderLayer::getVignette, VIGNETTE_TEXTURE, 0, 0, 0.0F, 0.0F,
+                VIGNETTE_TEXTURE,
+                0,
+                0,
+                -90,
+                0.0F,
+                0.0F,
                 context.getScaledWindowWidth(),
                 context.getScaledWindowHeight(),
                 context.getScaledWindowWidth(),
-                context.getScaledWindowHeight(),
-                i
+                context.getScaledWindowHeight()
         );
+        RenderSystem.depthMask(true);
+        RenderSystem.enableDepthTest();
+        context.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableBlend();
         ci.cancel();
     }
 }
