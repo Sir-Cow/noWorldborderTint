8aed4e169c5cb6d9a5211fc85f8070fc933856a1
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2025-01-26 11:12:44 +0000

    multiloader convert
    convert project to multiloader structure

---
+++ common/src/main/java/sircow/noworldbordertint/mixin/GuiMixin.java
@@ -0,0 +1,41 @@
+package sircow.noworldbordertint.mixin;
+
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ARGB;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public class GuiMixin {
+    @Shadow
+    public float vignetteBrightness;
+    @Shadow @Final
+    private static ResourceLocation VIGNETTE_LOCATION;
+    @Inject(method = "renderVignette", at = @At("HEAD"), cancellable = true)
+    private void sir_cow$modifyVignette(GuiGraphics guiGraphics, Entity entity, CallbackInfo ci) {
+        int i;
+        float h = this.vignetteBrightness;
+        h = Mth.clamp(h, 0.0F, 1.0F);
+        i = ARGB.colorFromFloat(1.0F, h, h, h);
+
+        guiGraphics.blit(
+                RenderType::vignette,
+                VIGNETTE_LOCATION, 0, 0, 0.0F, 0.0F,
+                guiGraphics.guiWidth(),
+                guiGraphics.guiHeight(),
+                guiGraphics.guiWidth(),
+                guiGraphics.guiHeight(),
+                i
+        );
+        ci.cancel();
+    }
+}
---
+++ fabric/src/main/java/sircow/noworldbordertint/NoWorldborderTint.java
@@ -0,0 +1,10 @@
+package sircow.noworldbordertint;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoWorldborderTint implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
